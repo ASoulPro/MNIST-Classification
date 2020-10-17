b4f90f2df17c336375eecfeedf8e44d805c4fad7
ASoulPro <Sulaimann.S@protonmail.com>
2020-10-17 19:13:04 +0300

    Init Commit

---
+++ MNSIT.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+input_size = 28*28
+hidden_size = 100
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+examples = iter(train_loader)
+samples, labels = examples.next()
+print(samples.shape, labels.shape)
